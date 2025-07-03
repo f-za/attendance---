131f185d47f93f2bd163dcfad3cb36c585e5aa87
f-za <wda978079@gmail.com>
2025-07-03 11:12:48 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDk7Y-FS3tPBCYB8UPzvLnODMocV-iHLQI",
+  authDomain: "attendance-1cbdf.firebaseapp.com",
+  projectId: "attendance-1cbdf",
+  storageBucket: "attendance-1cbdf.firebasestorage.app",
+  messagingSenderId: "428674326610",
+  appId: "1:428674326610:web:25f46045e210bd362d3f8b"
+};
