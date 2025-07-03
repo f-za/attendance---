d4254eb27ece4110b9cf10147330321646ebcba5
f-za <wda978079@gmail.com>
2025-07-03 12:57:02 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDk7Y-FS3tPBCYB8UPzvLnODMocV-iHLQI",
   authDomain: "attendance-1cbdf.firebaseapp.com",
   projectId: "attendance-1cbdf",
-  storageBucket: "attendance-1cbdf.firebasestorage.app",
+  storageBucket: "attendance-1cbdf.appspot.com",
   messagingSenderId: "428674326610",
   appId: "1:428674326610:web:25f46045e210bd362d3f8b"
 };
